94c60aab396defb32a00d83a0aa964ef4e020ecf
rohit <rohit.880207@gmail.com>
2025-03-10 14:33:36 +0530

    added utils

---
+++ utils/colors.ts
@@ -0,0 +1,20 @@
+import { Appearance } from "react-native";
+
+const theme = {
+    dark: {
+        text: 'white',
+        background: '#23252D',
+        primary: '#00C896',
+        border: '#5E5E5E'
+    },
+    light: {
+        text: 'black',
+        background: '#FAFAFA',
+        primary: '#00C896',
+        border: '#dbdbdb'    
+
+    }
+  };
+
+const isDarkMode = Appearance.getColorScheme() === 'dark';
+export const colors = theme[isDarkMode ? 'dark' : 'light'];
